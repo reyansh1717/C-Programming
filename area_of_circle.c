2fc44b57b813cf7a64240d32e2fede42c22e2bc0
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-03 22:47:19 +0530

    area_of_circle.c

---
+++ area_of_circle.c
@@ -0,0 +1,11 @@
+WAP to calculate area of a circle
+#include<stdio.h>
+int main()
+{
+    float Pi = 22/7, Radius,Area;
+    printf("Enter Radius of Circle: ");
+    scanf("%f",&Radius);
+    Area = Pi * Radius * Radius;
+    printf("Area of circle: %f\n",Area);
+    return 0;
+}
