7b4446928edfcb25579df2a7a88cea23f7a8249c
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-08 22:34:40 +0530

    area_of_triangle.c

---
+++ area_of_triangle.c
@@ -0,0 +1,13 @@
+WAP to calculate area of a triangle whose base and height is given.
+#include<stdio.h>
+int main()
+{
+    float Base,Height,Area;
+    printf("Enter Base of Triangle: ");
+    scanf("%f",&Base);
+    printf("Enter Height of Triangle: ");
+    scanf("%f",&Height);
+    Area = (Base * Height) / 2;
+    printf("Area of circle: %f\n",Area);
+    return 0;
+}
