d631412f8bfdb93d8b3d4d3a4f0f93fa924eb1a6
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-09 19:45:32 +0530

    swap_two_integer.c

---
+++ swap_two_integer.c
@@ -0,0 +1,16 @@
+WAP to swap two integer numbers using third variable.
+#include<stdio.h>
+int main()
+{
+    int A=5,B=10,temp;
+    printf("Before Swap\n");
+    printf("A : %d\n",A);
+    printf("B : %d\n",B);
+    temp=A;
+    A=B;
+    B=temp;
+    printf("After Swap\n");
+    printf("A : %d\n",A);
+    printf("B : %d\n",B);
+    return 0;
+}
