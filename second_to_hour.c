2eff4d6f2d92f2db5d5e6b660225d51e2792572f
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-12 21:43:56 +0530

    second_to_hour.c

---
+++ second_to_hour.c
@@ -0,0 +1,14 @@
+WAP to convert given second into its equivalent hour, minute and second as per the following
+format. Example. 7560 second = 2 Hour, 27 Minute and 40 Second
+#include<stdio.h>
+int main()
+{
+    int hour,minute,second,input;
+    printf("Enter Time in sec: ");
+    scanf("%d",&second);
+    hour= (second/3600);
+    minute=(second-(3600*hour))/60;
+    second=(second-(3600*hour)-(minute*60));
+    printf("Time: %d Hour, %d Minute and %d Second\n",hour,minute,second);
+    return 0;
+}
