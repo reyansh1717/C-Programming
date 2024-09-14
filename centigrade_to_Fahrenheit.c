1d6bb7123dd6bafd0d5d9ee6156ea7cb60643756
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-14 23:56:20 +0530

    centigrade_to_Fahrenheit.c

---
+++ centigrade_to_Fahrenheit.c
@@ -0,0 +1,11 @@
+WAP to convert temperature from centigrade to Fahrenheit scale.
+#include<stdio.h>
+int main()
+{
+    float Celsius, Fahrenheit;
+    printf("Enter the temperature in Celsius: ");
+    scanf("%f", &Celsius);
+    Fahrenheit = (9.0/5.0) * Celsius + 32;
+    printf("%f Celsius is %f Fahrenheit\n", Celsius, Fahrenheit);
+    return 0;
+}
