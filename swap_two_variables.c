d233c24e374693c3cd7af33befb21c9f1070db6a
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-22 00:20:50 +0530

    swap_two_variables.c

---
+++ swap_two_variables.c
@@ -0,0 +1,9 @@
+WAP swap the contents of two variables by using a single statement for swap in C.
+#include<stdio.h>
+int main()
+{
+    int x = 1, y = 2;
+    (x ^= y), (y ^= x), (x ^= y);
+    printf("a = %d, b = %d\n", x, y);
+    return 0;
+}
