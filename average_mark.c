80ffa62c21227e02aa58b6e68a50c6f2a143e159
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-23 00:10:17 +0530

    average_mark.c

---
+++ average_mark.c
@@ -0,0 +1,16 @@
+WAP to find the average mark of 5 subjects of a student and find the percentage. Assume full
+mark of each subject is 100.
+#include<stdio.h>
+int main()
+{
+    int sub1,sub2,sub3,sub4,sub5,total,average,percentage;
+    printf("Enter the marks of 5 subjects of a student:\n");
+    scanf("%d %d %d %d %d",&sub1,&sub2,&sub3,&sub4,&sub5);
+    total=sub1+sub2+sub3+sub4+sub5;
+    average=total/5;
+    percentage=(total*100)/500;
+    printf("Total marks of student is %d\n",total);
+    printf("Average marks of student is %d\n",average);
+    printf("Percentage of student is %d\n",percentage);
+    return 0;
+}
