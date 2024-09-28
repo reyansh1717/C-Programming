93fdb15f14d04e6b07b25fe2c44013db075dc40f
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-09-28 15:34:34 +0530

    leap_year.c

---
+++ leap_year.c
@@ -0,0 +1,29 @@
+4 WAP to determine whether a year entered through the keyboard is a leap year or not.
+#include<stdio.h>
+void leap(int year);
+void main()
+{
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+    leap(year);
+}
+void leap(int year)
+{
+        if(year%400==0)
+        {
+            printf("%d is a leap year.",year);
+        }
+        else if(year%100==0)
+        {
+            printf("%d is not a leap year.", year);
+        }
+        else if(year%4==0)
+        {
+            printf("%d is a leap year.", year);
+        }
+        else
+        {
+            printf("%d is not a leap year.", year);
+        }
+}
