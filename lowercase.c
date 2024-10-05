6e5ca295c54beb7770b7f88f4ad83cbc797248a8
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-05 22:13:23 +0530

    lowercase.c

---
+++ lowercase.c
@@ -0,0 +1,16 @@
+WAP to read an alphabet from the user and convert it into lowercase if the entered
+alphabet is in uppercase, otherwise display an appropriate message.
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    char upperChar, lowerChar;
+    int ascii;
+    printf("Enter an uppercase Character: ");
+    scanf("%c", &upperChar);
+    ascii = upperChar;
+    lowerChar = ascii+32;
+    printf("\nIts Lowercase = %c", lowerChar);
+    getch();
+    return 0;
+}
