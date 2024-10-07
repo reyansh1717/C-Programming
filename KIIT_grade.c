25fa8871b547ef11a848cb0d8271a8020fece2fa
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-07 13:42:21 +0530

    Create KIIT_grade.c

---
+++ KIIT_grade.c
@@ -0,0 +1,38 @@
+WAP to display the grade system of KIIT University based on total marks secured by a
+student in a semester. Use else..if ladder statement.
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("Enter total maks secured by a student\n");
+    scanf("%d", &marks);
+    if(marks >= 90)
+    {
+        printf("GRADE : O");
+    }
+    else if (marks >= 80 && marks < 90)
+    {
+        printf("GRADE : E");
+    }
+    else if (marks >= 70 && marks < 80)
+    {
+        printf("GRADE : A");
+    }
+    else if (marks >= 60 && marks < 70)
+    {
+        printf("GRADE : B");
+    }
+    else if (marks >= 50 && marks < 60)
+    {
+        printf("GRADE : C");
+    }
+    else if (marks >= 40 && marks < 50)
+    {
+        printf("GRADE : D");
+    }
+    else
+    {
+        printf("YOUR GRADE : Fail\n");
+    }
+    return(0);
+}
