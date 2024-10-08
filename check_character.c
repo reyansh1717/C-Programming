dd9e479366dfa5f42b17a3b46b95338940e07181
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-08 13:12:18 +0530

    check_character.c

---
+++ check_character.c
@@ -0,0 +1,22 @@
+WAP to check whether a character entered through keyboard is a digit, letter, special character
+etc or not.
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("'%c' is alphabet.", ch);
+    }
+    else if(ch >= '0' && ch <= '9')
+    {
+        printf("'%c' is digit.", ch);
+    }
+    else 
+    {
+        printf("'%c' is special character.", ch);
+    }
+    return 0;
+}
