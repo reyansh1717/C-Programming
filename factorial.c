4e776bcc0d49678803049db56e298a0aaabfc4a3
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-12 13:41:23 +0530

    factorial.c

---
+++ factorial.c
@@ -0,0 +1,13 @@
+WAP to calculate the factorial of a given number.
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    long int fact=1;
+    printf("\nEnter A Number To Find Out Its Factorial= ");
+    scanf("%d",&n);
+    for (i=n;i>0;i--)
+    fact=fact*i;
+    printf("\n%d!=%ld\n",n,fact);
+    return 0;
+}
