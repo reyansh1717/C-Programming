de900c3667fddd3f0947fda814c4e1704b379a7d
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-13 13:11:13 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,17 @@
+WAP to calculate the sum of digits of a given number.
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0,d,num;
+    printf("\nEnter a number: ");
+    scanf("%d",&n);
+    num=n;
+    while(n!=0)
+    {
+        d=n%10;
+        sum=sum+d;
+        n=n/10;
+    }
+    printf("\nSum of digits of the number %d is = %d",num,sum);
+    return 0;
+}
