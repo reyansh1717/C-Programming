646b8b807c71e8c47d6885ef7a4bc2e4a977df49
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-14 20:57:25 +0530

    hcf.c

---
+++ hcf.c
@@ -0,0 +1,19 @@
+WAP to find the GCD/HCF of two numbers.
+#include<stdio.h>
+int main()
+{
+    int a,b,m,n,gcd;
+    printf("\nEnter two numbers = ");
+    scanf("%d%d",&a,&b);
+    m=a;n=b;
+    while(a!=b)
+    {
+        if(a>b)
+        a=a-b;
+        else
+        b=b-a;
+    }
+    gcd=b;
+    printf("\nThe HCF of %d and %d is %d\n",m,n,gcd);
+    return 0;
+}
