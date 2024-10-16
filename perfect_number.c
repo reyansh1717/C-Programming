297d2827fbd743a43ac3b2ba432e657a04cb2667
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-16 19:54:18 +0530

    Create perfect_number.c

---
+++ perfect_number.c
@@ -0,0 +1,18 @@
+WAP to check whether an input integer is perfect number or not.
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("\nEnter Any Number = ");
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        sum+=i;
+    }
+    if(n==sum)
+    printf("\n%d is a perfect number",n);
+    else
+    printf("\n%d is not a perfect number",n);
+    return 0;
+}
