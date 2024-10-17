ea475cf4a533867d2bead276af138597ccc48410
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-17 16:15:43 +0530

    Create equation.c

---
+++ equation.c
@@ -0,0 +1,14 @@
+WAP to evaluate the equation y=xn where n is a non-negative integer.
+#include <stdio.h>
+int main()
+{
+    int n,i;float x,y=1.0;
+    printf("\nEnter values for x and n = ");
+    scanf("%f%d",&x,&n);
+    for(i=1;i<=n;i++)
+    {
+        y=y*x;
+    }
+    printf("\n%0.2f to the power %d is %0.2f",x,n,y);
+    return 0;
+}
