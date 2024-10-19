f3e47ddec36eab461830f0c004234e6b9ff9d22e
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-19 21:04:12 +0530

    Create ABC_pattern.c

---
+++ ABC_pattern.c
@@ -0,0 +1,18 @@
+WAP to print the following pattern for n rows. Ex. for n=5 rows
+A
+A B
+A B C
+A B C D
+A B C D E
+#include <stdio.h>
+int main()
+{
+    int i, j, n = 5;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%c ", 'A' + j - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
