3ba0b9c5dc4d897df1865ba09fba5612d61fc46c
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-20 09:13:55 +0530

    Create pattern_n.c

---
+++ pattern_n.c
@@ -0,0 +1,20 @@
+WAP to print the following pattern for n rows. Ex. for n=6 rows.
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+0 1 0 1 0 1
+
+LA4.1 WAP to print your name 5 times using while loop.
+#include<stdio.h>
+int main()
+{
+    int num_2105565 = 0;
+    while (num_2105565<5)
+    {
+        printf("\nREYANSH\n");
+        num_2105565+=1;
+    }
+    return 0;
+}
