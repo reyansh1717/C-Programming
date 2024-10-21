fbf35a14a155f2481cc32b5a84b8066cff735f8f
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-21 10:52:50 +0530

    Create series.c

---
+++ series.c
@@ -0,0 +1,12 @@
+WAP to print the series as 1 2 3 4 5 6 7 ..........100.
+#include<stdio.h>
+void main()
+{     
+    int i;
+	printf("series are:\n");
+	for (i=1;i<=100;i++)
+	{      
+		printf("%d ",i);
+	}
+printf("\n");
+}
