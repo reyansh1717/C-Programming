cb2652f7aa9bebca8936585eb6aa8ffa31e924da
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-21 10:52:22 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,14 @@
+WAP to input any integer and print your name that many times.
+#include<stdio.h>
+int main()
+{
+    int i = 0,n;
+    printf("\n Enter the no u want to display ur name : \n");
+    scanf("%d", &n);
+    while (i<n)
+    {
+        printf("\nREYANSH_2105565");
+        i++;
+    }
+    return 0;
+}
