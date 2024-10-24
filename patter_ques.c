c98702faf23f58a165d62813bf18bf83e8a263ff
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-10-24 13:34:34 +0530

    Create patter_ques.c

---
+++ patter_ques.c
@@ -0,0 +1,18 @@
+WAP to print the following pattern for n rows. Ex. for n=5 rows
+A
+B A
+C B A
+D C B A
+E D C B A
+#include<stdio.h>
+int main()
+{
+    int i, j, n = 5;
+    for (i = 1; i <= n; i++) {
+        for (j = i; j >= 1; j--) {
+            printf("%c ", 'A' + j - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
