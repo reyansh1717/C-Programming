c91b462072545d13a34793673efb742f880b9cc0
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-11-05 09:13:13 +0530

    Update alphabet_uppercase.c

--- alphabet_uppercase.c
+++ alphabet_uppercase.c
@@ -1,5 +1,6 @@
 WAP to read an alphabet from the user and convert it into uppercase if the entered alphabet is
 in lowercase, otherwise display an appropriate message.
+
 #include <stdio.h>
 #include <ctype.h>
 int main()
