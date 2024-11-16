34a7601ec88e3aa0e4f1cf2f242b1ddf22b7a0d9
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-11-17 01:59:39 +0530

    Create 10_array.c

---
+++ 10_array.c
@@ -0,0 +1,17 @@
+WAP to input 10 integers into an array of size 10. Print all elements.
+#include<stdio.h>
+int main()
+{
+    int arr[10];
+    printf("Enter values for the array:");
+    for (int i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("The elements are : ");
+    for(int i=0;i<10;i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+    return 0;
+}
