f7a8b374bfc065dfdb59361993ae55ca762f6753
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2024-12-01 19:37:21 +0530

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,37 @@
+WAP to find out the sum of the elements stored in a matrix.
+#include<stdio.h>
+int main()
+{
+    int m,n,sum=0;
+    printf("Enter number of rows of matrix : ");
+    scanf("%d",&m);
+    printf("Enter number of columns of matrix : ");
+    scanf("%d",&n);
+    int arr[m][n];
+    printf("Enter value of matrix : \n");
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("Given matrix : \n");
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            sum = sum +arr[i][j];
+        }
+    }
+    printf("The sum of all elements of the matrix is %d",sum);
+    return 0;
+}
