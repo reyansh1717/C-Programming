e0c3a711a2d5f54b2009660108459120fe54eb65
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2025-01-06 12:59:07 +0530

    Update check_character.c

--- check_character.c
+++ check_character.c
@@ -1,5 +1,5 @@
-WAP to check whether a character entered through keyboard is a digit, letter, special character
-etc or not.
+WAP to check whether a character entered through keyboard is a digit, letter, special character etc or not.
+
 #include<stdio.h>
 int main()
 {
