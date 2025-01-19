566297b32f22ebf208dfe357650b563793e8c800
Reyansh <135148145+reyansh1717@users.noreply.github.com>
2025-01-19 13:06:55 +0530

    Update meter_kilometer.c

--- meter_kilometer.c
+++ meter_kilometer.c
@@ -1,5 +1,5 @@
-WAP to convert a quantity in meter entered through keyboard into its equivalent kilometer and
-meter as per the following format. Example. 2430 meter = 2 Km and 430 meter.
+WAP to convert a quantity in meter entered through keyboard into its equivalent kilometer and meter as per the following format. Example. 2430 meter = 2 Km and 430 meter.
+
 #include<stdio.h>
 int main()
 {
